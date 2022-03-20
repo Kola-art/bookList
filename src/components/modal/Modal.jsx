2712420a1fd4079bd7813a7a98a746db17af88ac
Artem <73796530+Kola-art@users.noreply.github.com>
2022-03-20 21:08:39 +0200

    Update Modal.jsx

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../../styles/modal.css'
 
-const Modal = ({modal, setModal, addModal, setAddModal}) => {
+const Modal = ({modal, setModal}) => {
   let modalClass = ['modal'];
 
   const showModal = () => {
